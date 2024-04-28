91c9fc46eac9a52a3ba31ef1320e554353912574
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-04-28 13:56:32 -0700

    insertion_sort_list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - it should work
+ * @listint_t: para1
+ * @list: the list that should be sorted
+ * Description: it sorts an array in numberic order
+ * Return: sorted array
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *Current;
+	listint_t *Prev;
+	listint_t *temp;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+	Current = (*list)->next;
+	while (Current != NULL)
+	{
+		Prev = Current->prev;
+		while (Prev != NULL && Prev->n > Current->n)
+		{
+			temp = Prev->prev;
+			Prev->prev = Current;
+			Current->prev = temp;
+			if (temp)
+				temp->next = Current;
+			Prev->next = Current->next;
+			if (Current->next)
+				Current->next->prev = Prev;
+			Current->next = Prev;
+			if (Prev == *list)
+				*list = Current;
+			temp = *list;
+			while (temp != NULL)
+			{
+				printf("%d", temp->n);
+				if (temp->next != NULL)
+					printf(", ");
+				temp = temp->next;
+			}
+			printf("\n");
+			Prev = Current->prev;
+		}
+		Current = Current->next;
+	}
+}
